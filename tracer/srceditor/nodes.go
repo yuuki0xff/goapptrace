e4d3fbcf2126f344a1446f84b9daa712c884781f
yuuki <yuuki0xff@gmail.com>
2017-08-24 22:13:13 +0900

    sort NodeList.list when Format() called

--- tracer/srceditor/nodes.go
+++ tracer/srceditor/nodes.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"go/format"
 	"go/token"
+	"sort"
 )
 
 const DefaultArrayCap = 1000
@@ -28,6 +29,27 @@ type NodeList struct {
 	list    []Node
 }
 
+type NodeSorter []Node
+
+func (n NodeSorter) Len() int {
+	return len(n)
+}
+func (n NodeSorter) Swap(i, j int) {
+	n[i], n[j] = n[j], n[i]
+}
+func (n NodeSorter) Less(i, j int) bool {
+	return n.topos(i) < n.topos(j)
+}
+func (n NodeSorter) topos(i int) int {
+	switch node := n[i].(type) {
+	case *InsertNode:
+		return int(node.Pos)
+	case *DeleteNode:
+		return int(node.Pos)
+	}
+	panic("Unreachable")
+}
+
 func (nl *NodeList) Add(nodes ...Node) {
 	if nl.list == nil {
 		nl.list = make([]Node, 0, DefaultArrayCap)
@@ -38,6 +60,8 @@ func (nl *NodeList) Add(nodes ...Node) {
 func (nl *NodeList) Format() ([]byte, error) {
 	var buf bytes.Buffer
 	var pos token.Pos
+
+	sort.Sort(NodeSorter(nl.list))
 
 	buf.Grow(len(nl.OrigSrc) + DefaultBufferCap)
 	for _, node_ := range nl.list {
