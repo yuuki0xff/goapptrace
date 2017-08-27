530600d5d832990d3bc5d089beba1da6e6158edc
yuuki <yuuki0xff@gmail.com>
2017-08-28 07:23:03 +0900

    show svg-graph

--- static/index.ts
+++ static/index.ts
@@ -53,3 +53,60 @@ app.controller("viewerCtl", ($scope) => {
         $scope.graphs.push(new GraphWindow());
     }
 });
+
+app.directive("svgGraph", ($compile, $sce, $document) => {
+    // Query-string付きのURLで発生するエラーを止める
+    $sce.RESOURCE_URL = ['self'];
+
+    return {
+        restrict: 'E',
+        // TODO: templateURLを変更
+        templateUrl: "/api/log.svg?width=24000&height=800&layout=goroutine&color-rule=module&colors=6&start=1&scale=1.0",
+        link: (scope, element, attrs) => {
+            let svgElm: any = element.find("svg");
+
+            // position of started drag.
+            let startX = 0, startY = 0;
+            // position of viewBox
+            let vx = 0, vy = 0;
+            // viewBox size / viewPort size
+            let scale = 1.0;
+            // temporary position of viewBox
+            let x, y;
+
+            element.on('mousedown', function (event) {
+                // Prevent default dragging of selected content
+                event.preventDefault();
+                startX = event.pageX;
+                startY = event.pageY;
+                $document.on('mousemove', mousemove);
+                $document.on('mouseup', mouseup);
+            });
+
+            function mousemove(event) {
+                // raw moving distance
+                let dx = event.pageX - startX;
+                let dy = event.pageY - startY;
+                console.log(dx,dy);
+
+                // set viewBox
+                let w, h;
+                x = vx - dx * scale;
+                y = vy - dy * scale;
+                w = element[0].offsetWidth * scale;
+                h = element[0].offsetHeight * scale;
+                console.log(svgElm);
+                svgElm.attr('viewBox', [x, y, w, h].join(' '));
+            }
+
+            function mouseup() {
+                // update viewBox position
+                vx = x;
+                vy = y;
+
+                $document.off('mousemove', mousemove);
+                $document.off('mouseup', mouseup);
+            }
+        }
+    };
+});
