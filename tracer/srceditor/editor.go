yuuki0xff/goapptrace

commit e349d2f5072013ee4ad73e0b5222c45a4b38fc14

fix bug that prefix option does not work

tracer/srceditor/editor.go
@@ -16,6 +16,8 @@ type CodeEditor struct {
 	Prefix       string
 	Files        []string
 	Overwrite    bool
+
+	tmpl *Template
 }
 
 func (ce *CodeEditor) EditAll() error {
@@ -28,6 +30,8 @@ func (ce *CodeEditor) EditAll() error {
 }
 
 func (ce *CodeEditor) Edit(fname string) error {
+	ce.init()
+
 	edit := func(r io.Reader, w io.Writer) error {
 		src, err := ioutil.ReadAll(r)
 		if err != nil {
@@ -87,6 +91,21 @@ func AtomicReadWrite(fname string, fn func(r io.Reader, w io.Writer) error) erro
 	return os.Rename(tmpfname, fname)
 }
 
+func (ce *CodeEditor) init() {
+	if ce.tmpl == nil {
+		var importPrefix, varPrefix string
+		if ce.Prefix != "" {
+			importPrefix = ce.Prefix + "_import"
+			varPrefix = ce.Prefix + "_var_"
+		}
+
+		ce.tmpl = newTemplate(TemplateData{
+			ImportName:     importPrefix,
+			VariablePrefix: varPrefix,
+		})
+	}
+}
+
 func (ce *CodeEditor) edit(fname string, src []byte) ([]byte, error) {
 	nl := NodeList{OrigSrc: src}
 
@@ -109,13 +128,13 @@ func (ce *CodeEditor) edit(fname string, src []byte) ([]byte, error) {
 			wantImport = true
 			nl.Add(&InsertNode{
 				Pos: node.Body.Pos(),
-				Src: tmpl.render("funcStartStopStmt", nil),
+				Src: ce.tmpl.render("funcStartStopStmt", nil),
 			})
 		case *ast.FuncLit:
 			wantImport = true
 			nl.Add(&InsertNode{
 				Pos: node.Body.Pos(),
-				Src: tmpl.render("funcStartStopStmt", nil),
+				Src: ce.tmpl.render("funcStartStopStmt", nil),
 			})
 		}
 		return true
@@ -125,7 +144,7 @@ func (ce *CodeEditor) edit(fname string, src []byte) ([]byte, error) {
 	if wantImport {
 		nl.Add(&InsertNode{
 			Pos: f.Name.End(),
-			Src: tmpl.render("importStmt", nil),
+			Src: ce.tmpl.render("importStmt", nil),
 		})
 	}
 

cmd/trace_on.go
@@ -56,8 +56,8 @@ func runTraceOn(conf *config.Config, exportedOnly bool, prefix string, targetNam
 			editor := &srceditor.CodeEditor{
 				Files:        files,
 				ExportedOnly: exportedOnly,
-				Overwrite:    true,   //TODO
-				Prefix:       prefix, //TODO: does not work ....
+				Overwrite:    true, //TODO
+				Prefix:       prefix,
 			}
 			if err := editor.EditAll(); err != nil {
 				return err

tracer/srceditor/template.go
@@ -23,20 +23,10 @@ type Template struct {
 	t    *template.Template
 }
 
-var tmpl Template
+func newTemplate(data TemplateData) *Template {
+	var t Template
 
-func init() {
-	tmpl.init()
-	tmpl.add("importStmt", `
-		import {{.ImportName}} "{{.ImportPath}}"
-	`)
-	tmpl.add("funcStartStopStmt", `
-		{{.ImportName}}.FuncStart()
-		defer {{.ImportName}}.FuncEnd()
-	`)
-}
-
-func (t *Template) init() {
+	t.data = data
 	if t.data.ImportName == "" {
 		t.data.ImportName = DefaultImportName
 	}
@@ -52,6 +42,15 @@ func (t *Template) init() {
 	if t.t == nil {
 		t.t = &template.Template{}
 	}
+
+	t.add("importStmt", `
+		import {{.ImportName}} "{{.ImportPath}}"
+	`)
+	t.add("funcStartStopStmt", `
+		{{.ImportName}}.FuncStart()
+		defer {{.ImportName}}.FuncEnd()
+	`)
+	return &t
 }
 
 func (t *Template) add(name, tmplStr string) {

commit 599fff6b49b4b442180f661a4799c90ac7304551

fix a failed test
it is broken since e349d2f5072013ee4ad73e0b5222c45a4b38fc14

tracer/srceditor/editor.go
@@ -30,8 +30,6 @@ func (ce *CodeEditor) EditAll() error {
 }
 
 func (ce *CodeEditor) Edit(fname string) error {
-	ce.init()
-
 	edit := func(r io.Reader, w io.Writer) error {
 		src, err := ioutil.ReadAll(r)
 		if err != nil {
@@ -107,8 +105,9 @@ func (ce *CodeEditor) init() {
 }
 
 func (ce *CodeEditor) edit(fname string, src []byte) ([]byte, error) {
-	nl := NodeList{OrigSrc: src}
+	ce.init()
 
+	nl := NodeList{OrigSrc: src}
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, fname, src, parser.ParseComments)
 	if err != nil {