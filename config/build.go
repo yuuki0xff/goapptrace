708f2304501a6b942a938b36af91e647cdb911ec
yuuki <yuuki0xff@gmail.com>
2017-11-13 16:24:02 +0900

    split to runCmd and execCmd

--- config/build.go
+++ config/build.go
@@ -20,13 +20,20 @@ func (bp *BuildProcess) Run() (*exec.Cmd, error) {
 	return runCmd(args)
 }
 
+// runCmd execute a command and wait for exit
 func runCmd(args []string) (*exec.Cmd, error) {
+	cmd, err := execCmd(args)
+	if err != nil {
+		return nil, err
+	}
+	return cmd, cmd.Wait()
+}
+
+// execCmd execute a command but does not wait for exit
+func execCmd(args []string) (*exec.Cmd, error) {
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		return nil, err
-	}
-	return cmd, nil
+	return cmd, cmd.Start()
 }
