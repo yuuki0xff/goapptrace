500ee3e605dad4ada3052fbf5a1d7c820fc02b45
yuuki <yuuki0xff@gmail.com>
2017-11-17 14:33:45 +0900

    add comments

--- tracer/srceditor/template.go
+++ tracer/srceditor/template.go
@@ -43,18 +43,24 @@ func newTemplate(data TemplateData) *Template {
 		t.t = &template.Template{}
 	}
 
+	// "package"宣言の次の行に挿入される。
 	t.add("importStmt", `
 		import {{.ImportName}} "{{.ImportPath}}"
 	`)
+	// 関数の"{"の直後に挿入される。
+	// formatすると、関数の最初の行でFuncStart()を呼び出すようになる。
+	// "{"の後ろで開業されている場合、オリジナルのコードとの間には1行の空白が存在するはずである。
 	t.add("funcStartStopStmt", `
 		{{.VariablePrefix}}_txid := {{.ImportName}}.FuncStart()
 		defer {{.ImportName}}.FuncEnd({{.VariablePrefix}}_txid)
 	`)
+	// "funcStartStopStmt"と同様。ただし、mainパッケージのmain関数のみに適用される。
 	t.add("funcStartCloseStopStmt", `
 		{{.VariablePrefix}}_txid := {{.ImportName}}.FuncStart()
 		defer {{.ImportName}}.Close()
 		defer {{.ImportName}}.FuncEnd({{.VariablePrefix}}_txid)
 	`)
+	// os.Exit()の呼び出しを行う直前の行に挿入される。
 	t.add("close", "{{.ImportName}}.Close()\n")
 	return &t
 }
