5968fbab4ef37a5c65f756f954cf5afc41cc47c7
yuuki <yuuki0xff@gmail.com>
2018-01-01 16:34:17 +0900

    make spec file updatable

--- tracer/restapi/swagger-ui.sh
+++ tracer/restapi/swagger-ui.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 base=$(dirname $(readlink -f "$0"))
 exec docker run -it --rm \
-    -v $base/:/srv/ \
-    -e SWAGGER_JSON=/srv/api.yaml \
+    -v $base/:/usr/share/nginx/html/spec/ \
     -p 8080:8080 \
     swaggerapi/swagger-ui
