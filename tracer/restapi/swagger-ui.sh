2b70e68d5bf0f9808836312e6980b32014948c6d
yuuki <yuuki0xff@gmail.com>
2018-01-01 16:37:47 +0900

    open the Swagger UI automatically

--- tracer/restapi/swagger-ui.sh
+++ tracer/restapi/swagger-ui.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 base=$(dirname $(readlink -f "$0"))
+
+{
+    sleep 2
+    xdg-open 'http://localhost:8080/?url=./spec/api.yaml'
+} &
+
 exec docker run -it --rm \
     -v $base/:/usr/share/nginx/html/spec/ \
     -p 8080:8080 \
