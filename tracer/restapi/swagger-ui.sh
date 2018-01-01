84fbb778fa2d3946195d3f02feca623de9953e31
yuuki <yuuki0xff@gmail.com>
2018-01-01 15:47:48 +0900

    fix file name in SWAGGER_JSON parameter

--- tracer/restapi/swagger-ui.sh
+++ tracer/restapi/swagger-ui.sh
@@ -2,6 +2,6 @@
 base=$(dirname $(readlink -f "$0"))
 exec docker run -it --rm \
     -v $base/:/srv/ \
-    -e SWAGGER_JSON=/srv/api.json \
+    -e SWAGGER_JSON=/srv/api.yaml \
     -p 8080:8080 \
     swaggerapi/swagger-ui
