4f152dfca87b21024d9273fd641c7f67d43c0ed9
yuuki <yuuki0xff@gmail.com>
2018-01-04 16:29:23 +0900

    impl log server handlers

--- cmd/server_run.go
+++ cmd/server_run.go
@@ -23,13 +23,16 @@ package cmd
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 
 	"github.com/spf13/cobra"
 	"github.com/yuuki0xff/goapptrace/config"
 	"github.com/yuuki0xff/goapptrace/httpserver"
+	"github.com/yuuki0xff/goapptrace/tracer/logutil"
 	"github.com/yuuki0xff/goapptrace/tracer/protocol"
 	"github.com/yuuki0xff/goapptrace/tracer/restapi"
 	"github.com/yuuki0xff/goapptrace/tracer/storage"
@@ -97,9 +100,9 @@ func runServerRun(conf *config.Config, stdout io.Writer, stderr io.Writer, apiAd
 	// start Log Server
 	logSrv := protocol.Server{
 		Addr:    "tcp://" + logAddr,
-		Handler: protocol.ServerHandler{}, // TODO
-		AppName: "TODO",                   // TODO
-		Secret:  "",                       // TODO
+		Handler: getServerHandler(&strg),
+		AppName: "TODO", // TODO
+		Secret:  "",     // TODO
 	}
 	if err := logSrv.Listen(); err != nil {
 		fmt.Fprintln(stderr, "ERROR: failed to start the Log server:", err)
@@ -153,4 +156,81 @@ func init() {
 	// serverRunCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	serverRunCmd.Flags().StringP("listen-api", "p", "", "Address and port for REST API Server")
 	serverRunCmd.Flags().StringP("listen-log", "P", "", "Address and port for Log Server")
+}
+
+func getServerHandler(strg *storage.Storage) protocol.ServerHandler {
+	// workerとの通信用。
+	// close()されたら、workerは終了するべき。
+	var chMap map[protocol.ConnID]chan interface{}
+	// chanの追加、削除、close()するときはLock()を、chanへの送受信はRLock()をかける。
+	var chMapLock sync.RWMutex
+
+	worker := func(ch chan interface{}, id protocol.ConnID) {
+		logobj, err := strg.New()
+		if err != nil {
+			log.Panicf("ERROR: Server: failed to a create Log object: err=%s", err.Error())
+		}
+		defer func() {
+			if err = logobj.Close(); err != nil {
+				log.Panicf("failed to close a Log(%s): connID=%d err=%s", logobj.ID, id, err.Error())
+			}
+		}()
+		// TODO: rotateする前に、ssのfunclogsとgoroutinesを書き出す。書き出した後にClear()する
+		ss := logutil.StateSimulator{}
+		ss.Init()
+
+		for rawobj := range ch {
+			switch obj := rawobj.(type) {
+			case *logutil.RawFuncLog:
+				if err := logobj.AppendRawFuncLog(obj); err != nil {
+					log.Panicln("failed to append RawFuncLog:", err.Error())
+				}
+				ss.Next(*obj)
+			case *logutil.Symbols:
+				if err := logobj.AppendSymbols(obj); err != nil {
+					log.Panicln("failed to append Symbols:", err.Error())
+				}
+			default:
+				log.Panicf("unsupported type: %+v", rawobj)
+			}
+		}
+	}
+
+	return protocol.ServerHandler{
+		Connected: func(id protocol.ConnID) {
+			log.Println("INFO: Server: connected")
+
+			ch := make(chan interface{})
+			go worker(ch, id)
+
+			chMapLock.Lock()
+			chMap[id] = ch
+			chMapLock.Unlock()
+		},
+		Disconnected: func(id protocol.ConnID) {
+			log.Println("INFO: Server: disconnected")
+
+			chMapLock.Lock()
+			close(chMap[id])
+			delete(chMap, id)
+			chMapLock.Unlock()
+		},
+		Error: func(id protocol.ConnID, err error) {
+			log.Printf("ERROR: Server: connID=%d err=%s", id, err.Error())
+		},
+		Symbols: func(id protocol.ConnID, s *logutil.Symbols) {
+			log.Printf("DEBUG: Server: add symbols: %+v\n", s)
+
+			chMapLock.RLock()
+			chMap[id] <- s
+			chMapLock.RUnlock()
+		},
+		RawFuncLog: func(id protocol.ConnID, f *logutil.RawFuncLog) {
+			log.Printf("DEBUG: Server: got RawFuncLog: %+v\n", f)
+
+			chMapLock.RLock()
+			chMap[id] <- f
+			chMapLock.RUnlock()
+		},
+	}
 }
