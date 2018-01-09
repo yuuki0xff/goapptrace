3b0374867d46cb5ba0dc4990678ebefb5046bbc3
yuuki <yuuki0xff@gmail.com>
2018-01-09 15:52:36 +0900

    add wrapWidget struct

---
+++ logviewer/wrapwidget.go
@@ -0,0 +1,26 @@
+package logviewer
+
+import (
+	"image"
+
+	"github.com/marcusolsson/tui-go"
+)
+
+// 表示対象のWidgetを切り替えることが可能なWidget。
+type wrapWidget struct {
+	Widget tui.Widget
+}
+
+func (w *wrapWidget) SetWidget(widget tui.Widget) {
+	w.Widget = widget
+}
+
+func (w *wrapWidget) Draw(p *tui.Painter)                          { w.Widget.Draw(p) }
+func (w *wrapWidget) MinSizeHint() image.Point                     { return w.Widget.MinSizeHint() }
+func (w *wrapWidget) Size() image.Point                            { return w.Widget.Size() }
+func (w *wrapWidget) SizeHint() image.Point                        { return w.Widget.SizeHint() }
+func (w *wrapWidget) SizePolicy() (tui.SizePolicy, tui.SizePolicy) { return w.Widget.SizePolicy() }
+func (w *wrapWidget) Resize(size image.Point)                      { w.Widget.Resize(size) }
+func (w *wrapWidget) OnKeyEvent(ev tui.KeyEvent)                   { w.Widget.OnKeyEvent(ev) }
+func (w *wrapWidget) SetFocused(b bool)                            { w.Widget.SetFocused(b) }
+func (w *wrapWidget) IsFocused() bool                              { return w.Widget.IsFocused() }
