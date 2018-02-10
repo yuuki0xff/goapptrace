e28c9b77ad39c412bb32a5983afd132fe9a7027f
yuuki <yuuki0xff@gmail.com>
2018-02-10 18:20:11 +0900

    add Controller.theme()

--- logviewer/main.go
+++ logviewer/main.go
@@ -31,48 +31,7 @@ func (v *Controller) Run() error {
 	if err != nil {
 		return errors.Wrap(err, "failed to initialize TUI")
 	}
-	theme := tui.NewTheme()
-	theme.SetStyle("list.item.selected", tui.Style{Reverse: tui.DecorationOn})
-	theme.SetStyle("table.cell.selected", tui.Style{Reverse: tui.DecorationOn})
-	theme.SetStyle("button.focused", tui.Style{Reverse: tui.DecorationOn})
-	theme.SetStyle("label.error-message", tui.Style{
-		Fg:   tui.ColorRed,
-		Bold: tui.DecorationOn,
-	})
-	theme.SetStyle("label."+RunningStyleName, tui.Style{
-		Fg: tui.ColorYellow,
-	})
-	theme.SetStyle("line.gap", tui.Style{
-		Fg: tui.ColorBlue,
-	})
-	theme.SetStyle("line.stopped", tui.Style{
-		Fg: tui.ColorGreen,
-	})
-	theme.SetStyle("line.stopped.selected", tui.Style{
-		Fg:      tui.ColorGreen,
-		Bold:    tui.DecorationOn,
-		Reverse: tui.DecorationOn,
-	})
-	theme.SetStyle("line.stopped.marked", tui.Style{
-		Fg:   tui.ColorWhite,
-		Bg:   tui.ColorBlue,
-		Bold: tui.DecorationOn,
-	})
-	theme.SetStyle("line.running", tui.Style{
-		Fg:   tui.ColorGreen,
-		Bold: tui.DecorationOn,
-	})
-	theme.SetStyle("line.running.selected", tui.Style{
-		Fg:      tui.ColorGreen,
-		Bold:    tui.DecorationOn,
-		Reverse: tui.DecorationOn,
-	})
-	theme.SetStyle("line.running.marked", tui.Style{
-		Fg:   tui.ColorWhite,
-		Bg:   tui.ColorGreen,
-		Bold: tui.DecorationOn,
-	})
-	v.UI.SetTheme(theme)
+	v.UI.SetTheme(v.theme())
 
 	v.ctx, v.cancel = context.WithCancel(context.Background())
 	defer v.cancel()
@@ -114,4 +73,50 @@ func (v *Controller) setView(view View) {
 
 	v.view.Start(viewCtx)
 	go v.UI.Update(v.view.Update)
+}
+
+// theme returns default themes.
+func (v *Controller) theme() *tui.Theme {
+	theme := tui.NewTheme()
+	theme.SetStyle("list.item.selected", tui.Style{Reverse: tui.DecorationOn})
+	theme.SetStyle("table.cell.selected", tui.Style{Reverse: tui.DecorationOn})
+	theme.SetStyle("button.focused", tui.Style{Reverse: tui.DecorationOn})
+	theme.SetStyle("label.error-message", tui.Style{
+		Fg:   tui.ColorRed,
+		Bold: tui.DecorationOn,
+	})
+	theme.SetStyle("label."+RunningStyleName, tui.Style{
+		Fg: tui.ColorYellow,
+	})
+	theme.SetStyle("line.gap", tui.Style{
+		Fg: tui.ColorBlue,
+	})
+	theme.SetStyle("line.stopped", tui.Style{
+		Fg: tui.ColorGreen,
+	})
+	theme.SetStyle("line.stopped.selected", tui.Style{
+		Fg:      tui.ColorGreen,
+		Bold:    tui.DecorationOn,
+		Reverse: tui.DecorationOn,
+	})
+	theme.SetStyle("line.stopped.marked", tui.Style{
+		Fg:   tui.ColorWhite,
+		Bg:   tui.ColorBlue,
+		Bold: tui.DecorationOn,
+	})
+	theme.SetStyle("line.running", tui.Style{
+		Fg:   tui.ColorGreen,
+		Bold: tui.DecorationOn,
+	})
+	theme.SetStyle("line.running.selected", tui.Style{
+		Fg:      tui.ColorGreen,
+		Bold:    tui.DecorationOn,
+		Reverse: tui.DecorationOn,
+	})
+	theme.SetStyle("line.running.marked", tui.Style{
+		Fg:   tui.ColorWhite,
+		Bg:   tui.ColorGreen,
+		Bold: tui.DecorationOn,
+	})
+	return theme
 }
