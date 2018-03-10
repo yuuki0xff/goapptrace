e8c5e8137db853a783287e65ab12eb2d2fa83840
yuuki <yuuki0xff@gmail.com>
2018-03-10 23:14:58 +0900

    change Sender interface

--- tracer/logger/sender.go
+++ tracer/logger/sender.go
@@ -6,5 +6,12 @@ import "github.com/yuuki0xff/goapptrace/tracer/logutil"
 type Sender interface {
 	Open() error
 	Close() error
-	Send(diff *logutil.SymbolsData, raw *logutil.RawFuncLog) error
+	// SymbolsDataをサーバに送信する。
+	// この関数の実行終了後はdataの変更や破棄をしても構わない。
+	// SendSymbols()は、関数の実行終了までにdataが変更されても構わない状態にしなければならない。
+	SendSymbols(data *logutil.SymbolsData) error
+	// RawFuncLogをサーバに送信する。
+	// この関数の実行終了後はrawの変更や破棄をしても構わない。
+	// SendLog()は、関数の実行終了までにrawが変更されても構わない状態にしなければならない。
+	SendLog(raw *logutil.RawFuncLog) error
 }
