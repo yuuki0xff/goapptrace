0337aff69ca6dbb8ae67da2e7fa3b4a2f5ba7fc2
yuuki <yuuki0xff@gmail.com>
2018-03-11 00:04:43 +0900

    improve RetrySender. I forgot the details ...

--- tracer/logger/retrysender.go
+++ tracer/logger/retrysender.go
@@ -15,48 +15,61 @@ type RetrySender struct {
 }
 
 func (s *RetrySender) Open() error {
-	return s.autoretry(s.Sender.Open)
+	return s.retry("Open", s.Sender.Open)
 }
 func (s *RetrySender) Close() error {
-	return s.autoretry(s.Sender.Close)
+	return s.retry("Close", s.Sender.Close)
+}
+
+func (s *RetrySender) SendSymbols(data *logutil.SymbolsData) error {
+	return s.retrySend("SendSymbols", func() error {
+		// try to send
+		return s.Sender.SendSymbols(data)
+	})
 }
 
 // SendLog sends a RawFuncLog.
 // if occur the any error, retry to send after re-open.
 func (s *RetrySender) SendLog(raw *logutil.RawFuncLog) error {
-	return s.autoretry(func() error {
-		// try to send
-		senderr := s.Sender.SendLog(raw)
-		if senderr == nil {
-			return nil
-		}
-		log.Printf("failed to RetrySender.Send(): %s", senderr)
-
-		// try to close.
-		// if occurs any error, we print of logging message.
-		closeerr := s.Sender.Close()
-		if closeerr != nil {
-			log.Printf("failed to Sender.Close() on RetrySender.Send(): %s", closeerr)
-		}
-
-		// try to re-open.
-		openerr := s.Open()
-		if openerr != nil {
-			log.Panicf("failed to Sender.Open() on RetrySender.Send(): %s", openerr)
-			return openerr
-		}
-		return senderr
+	return s.retrySend("SendLog", func() error {
+		return s.Sender.SendLog(raw)
 	})
 }
-
-// autoretry is automatically retry until reached to retry limit or fn() is succeed.
-func (s *RetrySender) autoretry(fn func() error) error {
+func (s *RetrySender) retry(funcName string, fn func() error) error {
 	var err error
 	for i := 0; i < s.MaxRetry; i++ {
 		err = fn()
 		if err == nil {
 			return nil
 		}
+		log.Printf("failed to Sender.%s() on RetrySender.%s(): %s", funcName, funcName, err)
 	}
 	return err
+}
+
+// autoretry is automatically retry until reached to retry limit or fn() is succeed.
+func (s *RetrySender) retrySend(funcName string, send func() error) error {
+	var senderr error
+	for i := 0; i < s.MaxRetry; i++ {
+		senderr = send()
+		if senderr == nil {
+			return nil
+		}
+		log.Printf("failed to RetrySender.%s(): %s", funcName, senderr)
+
+		// try to close.
+		// if occurs any error, we print of logging message.
+		closeerr := s.Sender.Close()
+		if closeerr != nil {
+			log.Printf("failed to Sender.Close() on RetrySender.%s(): %s", funcName, closeerr)
+		}
+
+		// try to re-open.
+		openerr := s.Open()
+		if openerr != nil {
+			log.Panicf("failed to Sender.Open() on RetrySender.%s(): %s", funcName, openerr)
+			return openerr
+		}
+	}
+	return senderr
 }
