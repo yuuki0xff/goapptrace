437f85b650369a19fc9202baa87e4d53b555b11c
yuuki <yuuki0xff@gmail.com>
2018-03-13 16:41:34 +0900

    move types related to log records

--- tracer/logutil/struct.go
+++ tracer/logutil/struct.go
@@ -19,7 +19,7 @@ type StateSimulator struct {
 	// 次に追加するFuncLogのID
 	nextID schema.FuncLogID
 	// 実行中か実行が終了した関数についてのログ
-	funcLogs map[schema.FuncLogID]*FuncLog
+	funcLogs map[schema.FuncLogID]*schema.FuncLog
 	// RawFuncLog.TxIDに対応するFuncLogIDを保持する。
 	// 関数の実行が終了したら、そのTxIDを削除すること。
 	txids map[schema.TxID]schema.FuncLogID
@@ -29,7 +29,7 @@ type StateSimulator struct {
 	// 実行中か実行が終わったgoroutine
 	// 実行終了したと判断したgoroutineを動作中に変更することがあるので、
 	// 実行が終了しても削除してはならない。
-	goroutines map[schema.GID]*Goroutine
+	goroutines map[schema.GID]*schema.Goroutine
 
 	lock sync.RWMutex
 }
@@ -37,41 +37,4 @@ type StateSimulator struct {
 type StateSimulatorStore struct {
 	lock sync.Mutex
 	m    map[string]*StateSimulator
-}
-
-// Goroutineの生存期間、およびそのGoroutine内で行われたアクションを保持する。
-// 実行終了後も、変更されることがある。
-type Goroutine struct {
-	GID       schema.GID
-	StartTime schema.Time
-	EndTime   schema.Time
-}
-
-// 1回の関数呼び出しに関する情報。
-// 関数の生存期間、呼び出し元の関数など。
-// 関数の実行終了後は、フィールドの値は変更されない。
-type FuncLog struct {
-	ID        schema.FuncLogID
-	StartTime schema.Time
-	EndTime   schema.Time
-	ParentID  schema.FuncLogID
-
-	Frames []uintptr
-	GID    schema.GID
-}
-
-type RawFuncLog struct {
-	// TODO: ID fieldに適切な値を書き込む
-	// TODO: ドキュメントを書く
-
-	ID        schema.RawFuncLogID
-	Tag       schema.TagName `json:"tag"`
-	Timestamp schema.Time    `json:"timestamp"`
-	Frames    []uintptr      `json:"frames"` // Frames[0] is current frame, Frames[1] is the caller of Frame[0].
-	GID       schema.GID     `json:"gid"`
-	TxID      schema.TxID    `json:"txid"`
-}
-
-func (fl FuncLog) IsEnded() bool {
-	return fl.EndTime != schema.NotEnded
 }
---
+++ tracer/schema/log.go
@@ -0,0 +1,38 @@
+package schema
+
+// Goroutineの生存期間、およびそのGoroutine内で行われたアクションを保持する。
+// 実行終了後も、変更されることがある。
+type Goroutine struct {
+	GID       GID
+	StartTime Time
+	EndTime   Time
+}
+
+// 1回の関数呼び出しに関する情報。
+// 関数の生存期間、呼び出し元の関数など。
+// 関数の実行終了後は、フィールドの値は変更されない。
+type FuncLog struct {
+	ID        FuncLogID
+	StartTime Time
+	EndTime   Time
+	ParentID  FuncLogID
+
+	Frames []uintptr
+	GID    GID
+}
+
+type RawFuncLog struct {
+	// TODO: ID fieldに適切な値を書き込む
+	// TODO: ドキュメントを書く
+
+	ID        RawFuncLogID
+	Tag       TagName   `json:"tag"`
+	Timestamp Time      `json:"timestamp"`
+	Frames    []uintptr `json:"frames"` // Frames[0] is current frame, Frames[1] is the caller of Frame[0].
+	GID       GID       `json:"gid"`
+	TxID      TxID      `json:"txid"`
+}
+
+func (fl FuncLog) IsEnded() bool {
+	return fl.EndTime != NotEnded
+}
