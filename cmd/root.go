4b5367fab5a467f39d8479a629eb7a08c5d342a8
yuuki <yuuki0xff@gmail.com>
2018-03-26 01:27:11 +0900

    silence unnecessary messages

--- cmd/root.go
+++ cmd/root.go
@@ -39,10 +39,12 @@ var RootCmd = &cobra.Command{
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
+
+	// Silence unnecessary messages.
+	SilenceErrors: true,
+	SilenceUsage:  true,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() int {
 	if err := RootCmd.Execute(); err != nil {
 		log.Println(err)
