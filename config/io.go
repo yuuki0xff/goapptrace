88d11269066e2ba36c5ea59a496414f82fa3623d
yuuki <yuuki0xff@gmail.com>
2018-04-08 12:23:48 +0900

    update comment

--- config/io.go
+++ config/io.go
@@ -19,6 +19,7 @@ const (
 
 // Directory Layout
 //   $dir/targets.json        - includes target, trace, build
+//   $dir/servers.json        - list of server address.
 //   $dir/logs/               - managed under tracer.storage
 
 type Config struct {
