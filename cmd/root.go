cb276d41cd9433a479dd2d118cc125b72f5f9331
yuuki <yuuki0xff@gmail.com>
2018-06-26 03:16:15 +0900

    remove an unused flag

--- cmd/root.go
+++ cmd/root.go
@@ -61,7 +61,7 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in ENV variables if set.
